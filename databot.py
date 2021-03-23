4c9e64aa19286ff9ce214c9a7f7767896cfb3081
krjiang <kevinrenjiang@gmail.com>
2021-03-23 15:37:37 -0400

    added 'inhouse' command and adjusted gitignore

--- databot.py
+++ databot.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import random
 import discord
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-client = discord.Client()
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
 
 # shows that the bot is active
 @client.event
@@ -46,10 +48,36 @@ async def on_message(message):
             file_location = 'data.csv'
             data.to_csv(file_location)
 
+        # INHOUSE command: checks to see which channel the user is in and assigns random teams based
+        #                  on the users in the channel, tells you to use !reroll for new teams
+        if cmd == 'inhouse':
+            voiceChannel = message.author.voice.channel
+            textChannel = message.channel
+            channelMembers = voiceChannel.members
+            print(channelMembers)
+            random.shuffle(channelMembers)
+            teamsize = len(channelMembers) // 2
+            team1 = channelMembers[teamsize:]
+            team2 = channelMembers[:teamsize]
+            permutations = [team1]
+
+            team1 = [member.name for member in team1]
+            team2 = [member.name for member in team2]
+            string = 'TEAM 1: \n'
+            for member in team1:
+                string += member + '\n'
+            string += '\n TEAM 2: \n'
+            for member in team2:
+                string += member + '\n'
+            
+            await textChannel.send(string)
+            
+'''
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
         general = client.get_channel(822902651048951841)
         await general.send('hello ' + member.name)
+'''
 
 client.run('token')
